0388b74df35038fc2223703c74d68722a67c516b
Widas <widas@Widass-MacBook-Pro.local>
2019-04-25 14:14:24 +0700

    tambah inafraud

--- inafraudelection.R
+++ inafraudelection.R
@@ -1,3 +1,5 @@
 library(readr)
 indonesiafraudelection <- read_delim("raw_data/indonesiafraudelection.csv", 
                                      ";", escape_double = FALSE, trim_ws = TRUE)
+
+# hhh
